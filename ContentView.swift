d6b6f682c90339d4f51f99a4b410814c436e7d36
Aadi Shha <ashah8399@stu.d214.org>
2025-04-04 09:07:43 -0700

    added button

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color1 : Color = .black
     var body: some View {
         Text("Devin")
         Text("Aadi")
+        Button{
+            color1 = .red
+        } label:{
+            Text("Color")
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 100)
+                .background(color1)
+        }
     }
 }
